9be5df154e1f01baa6ad9730a3e1f96b80e47868
Chefmine8 <66253357+Chefmine8@users.noreply.github.com>
2022-10-17 08:16:40 +0200

    Add files via upload

--- j1.py
+++ j1.py
@@ -1,13 +1,12 @@
 import pygame
 
-class ball:
+class j1:
     def __init__(self, x, y):
-        self.image = pygame.image.load("./image/ball.jpg")
-        self.angle = pygame.transform.rotate(self.image, 0)
+        self.image = pygame.image.load("./image/raquette.png")
         self.rect = self.image.get_rect(x=x, y=y)
         self.speed = 1
         self.x = 0
         self.y = 0
 
     def draw(self, screen):
-        screen.blit(self.angle, (self.x, self.y))
+        screen.blit(self.image, (self.x, self.y))
--- run.py
+++ run.py
@@ -1,19 +1,35 @@
 import pygame
 import random
 from ball import ball
+from j1 import j1
+from j2 import j2
 
 class Game:
     def __init__(self, screen):
         self.screen = screen
         self.pause = False
         self.running = True
+        self.j1 = j1(0, 0)
+        self.j2 = j2(0, 0)
         self.ball = ball(0, 0)
-        self.ball.x = random.randrange(30, 1240, 30)
-        self.ball.y = random.randrange(30, 680, 30)
-        self.score = 0
+        self.j1.x = 0
+        self.j1.y = 540
+        self.j2.x = 1915
+        self.j2.y = 540
+        self.ball.x = random.randrange(680, 1240, 1)
+        self.ball.y = random.randrange(440, 640, 1)
+        while self.ball.x == 960:
+            self.ball.x = random.randrange(30, 1240, 1)
+        if self.ball.x > 960:
+            self.ball_x = 1
+        else :
+            self.ball_x = 2
+        self.score_j1 = 0
+        self.score_j2 = 0
         self.f = open('./option.txt', "r")
         self.best = int(self.f.read())
         self.f.close()
+        self.rotate = 0
 
     def handling_events(self):
 
@@ -21,16 +37,15 @@ class Game:
             if event.type == pygame.QUIT:
                 self.running = False
         keys= pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            pass
-        if keys[pygame.K_RIGHT]:
-            pass
+        if keys[pygame.K_s]:
+            self.j1.y += 3
+        if keys[pygame.K_z]:
+            self.j1.y -= 3
         if keys[pygame.K_UP]:
-            pass
+            self.j2.y -= 3
         if keys[pygame.K_DOWN]:
-            pass
+            self.j2.y += 3
         if keys[pygame.K_SPACE]:
-            time.sleep(self.snake.speed)
             self.pause = True
             while self.pause == True:
                 for event in pygame.event.get():
@@ -45,14 +60,33 @@ class Game:
                 pygame.display.flip()
 
     def update(self):
-        pass
+        if self.ball_x == 1:
+            self.ball.x += 2
+        elif self.ball_x == 2:
+            self.ball.x -= 2
 
+        if self.ball_y == 1:
+            self.ball
+
+        if self.ball.x > 1920 or self.ball.x < 0:
+            self.__init__(screen)
+
+        self.color = screen.get_at((self.ball.x, self.ball.y))
+
+        if self.color == (255, 255, 255):
+            print(self.color)
+            print(self.ball.angle)
+            pygame.mixer.music.load('./sound/touch.mp3')
+            pygame.mixer.music.play()
 
     def display(self):
         self.screen.fill("#000000")
-        self.create_message('big', 'Score :{}'.format(str(self.score)), (10, 10, 100, 50), (255,255,255))
-        self.create_message('big', 'Best score :{}'.format(str(self.best)), (10, 50, 100, 50), (255, 255, 255))
+        self.create_message('big', 'Score :{}'.format(str(self.score_j1)), (430, 10, 100, 50), (255,255,255))
+        self.create_message('big', 'Score :{}'.format(str(self.score_j2)), (1310, 10, 100, 50), (255,255,255))
+        self.create_message('big', 'Best score :{}'.format(str(self.best)), (860, 50, 100, 50), (255, 255, 255))
         self.ball.draw(screen)
+        self.j1.draw(screen)
+        self.j2.draw(screen)
         pygame.display.flip()
 
     def create_message(self, font, message, message_rectangle, color):
